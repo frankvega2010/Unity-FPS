71a7d68251002f9062c1a3611f1cbe2d00ffdc5b
Franco <franco_vega@msn.com>
2019-04-08 20:16:22 -0300

    Added FPS Camera and Static Trap

---
+++ Unity-FPS/Assets/Scripts/Enemy/AcidFloor.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcidFloor : MonoBehaviour
+{
+    //public GameObject acid;
+    public GameObject player;
+    public GameObject playerHitbox;
+    public GameObject playerHp;
+    public int damage;
+
+    private bool damageOnce = false;
+    //private Rigidbody playerRig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //playerRig = player.GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider playerHitbox)
+    {
+        //Debug.Log("piso");
+
+        if (playerHitbox.gameObject.tag == "Player")
+        {
+            if (!damageOnce)
+            {
+                playerHp.GetComponent<PlayerHealth>().health = playerHp.GetComponent<PlayerHealth>().health - 50;
+                player.GetComponent<CharacterController>().enabled = false;
+                player.transform.position = new Vector3(50, 50, 50);
+                player.GetComponent<CharacterController>().enabled = true;
+                //playerRig.AddForce(Vector3.back * 2, ForceMode.Impulse);
+                damageOnce = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider playerHitbox)
+    {
+        if (playerHitbox.gameObject.tag == "Player")
+        {
+            if (damageOnce)
+            {
+                damageOnce = false;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
