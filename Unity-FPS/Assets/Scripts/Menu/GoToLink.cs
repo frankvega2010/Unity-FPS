079c2c8afb9d5780e3d61dfd1c17b469acc36545
Franco <franco_vega@msn.com>
2019-04-13 23:36:32 -0300

    Made Credits Scene

---
+++ Unity-FPS/Assets/Scripts/Menu/GoToLink.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToLink : MonoBehaviour
+{
+    public string url;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void GotoWebsite()
+    {
+        Application.OpenURL(url);
+        Debug.Log("going to " + url);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
