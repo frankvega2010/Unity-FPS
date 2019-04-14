9d2b6804799aa497162000f3bd6a0996a8ba9a7f
Franco <franco_vega@msn.com>
2019-04-14 03:01:43 -0300

    Added GhostHunter weapon

---
+++ Unity-FPS/Assets/Scripts/Weapons/ActiveWeapon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveWeapon : MonoBehaviour
+{
+    public GameObject waterGun_prop;
+    public GameObject waterGun;
+    public GameObject ghostHunter_prop;
+    public GameObject ghostHunter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            waterGun.SetActive(true);
+            waterGun_prop.SetActive(true);
+            ghostHunter.SetActive(false);
+            ghostHunter_prop.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            waterGun.SetActive(false);
+            waterGun_prop.SetActive(false);
+            ghostHunter.SetActive(true);
+            ghostHunter_prop.SetActive(true);
+        }
+    }
+}
---
+++ Unity-FPS/Assets/Scripts/Weapons/GhostHunter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostHunter : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball.GetComponent<MeshRenderer>().enabled = false;
+        ball.GetComponent<SphereCollider>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ball.SetActive(true);
+            GameObject newBall = Instantiate(ball, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+            Rigidbody newBallRigidbody = newBall.GetComponent<Rigidbody>();
+            newBall.GetComponent<MeshRenderer>().enabled = true;
+            newBall.GetComponent<SphereCollider>().enabled = true;
+
+
+            newBall.transform.position = transform.position + Camera.main.transform.forward * 2.0f;
+            newBallRigidbody.velocity = Camera.main.transform.forward * 40;
+            //rig.AddForce(transform.position - dir * -7000);
+            //newBallRigidbody.AddForce(dir * 5, ForceMode.Impulse);
+            //hit.transform.gameObject.GetComponent<AcidFloor>().respawn = true;
+            //points.GetComponent<PlayerPoints>().points = points.GetComponent<PlayerPoints>().points + 100;
+            //hit.transform.gameObject.SetActive(false);
+        }
+    }
+}
