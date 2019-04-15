8336754f60698eaf8cabfe5dcf945f1e8bd5ddcd
Franco <franco_vega@msn.com>
2019-04-14 23:17:59 -0300

    Improved Level templates

--- Unity-FPS/Assets/Scripts/Level/RandomLevel.cs
+++ Unity-FPS/Assets/Scripts/Level/RandomLevel.cs
@@ -11,34 +11,63 @@ public class RandomLevel : MonoBehaviour
     {
         if (levelTemplate)
         {
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 8; i++)
             {
                 GameObject newLevel = Instantiate(levelTemplate, new Vector3(20 * i, levelTemplate.transform.position.y, 0), Quaternion.identity);
+                int whichLevel = 0;
                 
 
-                int whichLevel = Random.Range(1, 4);
-                newLevel.transform.Find("LevelTemplate" + whichLevel).gameObject.transform.Find("Acid").gameObject.GetComponent<AcidFloor>().iterationMultiplier = i + 1;
-                newLevel.transform.Find("LevelTemplate" + whichLevel).gameObject.transform.Find("Ghost").gameObject.GetComponent<Ghost>().iterationMultiplier = i + 1;
-                switch (whichLevel)
+                if (i == 0)
                 {
-                    case 1:
-                        newLevel.transform.Find("LevelTemplate3").gameObject.SetActive(false);
-                        newLevel.transform.Find("LevelTemplate2").gameObject.SetActive(false);
-                        newLevel.transform.Find("LevelTemplate1").gameObject.SetActive(true);
-                        break;
-                    case 2:
-                        newLevel.transform.Find("LevelTemplate3").gameObject.SetActive(false);
-                        newLevel.transform.Find("LevelTemplate1").gameObject.SetActive(false);
-                        newLevel.transform.Find("LevelTemplate2").gameObject.SetActive(true);
-                        break;
-                    case 3:
-                        newLevel.transform.Find("LevelTemplate3").gameObject.SetActive(true);
-                        newLevel.transform.Find("LevelTemplate2").gameObject.SetActive(false);
-                        newLevel.transform.Find("LevelTemplate1").gameObject.SetActive(false);
-                        break;
-                    default:
-                       break;
+                    newLevel.transform.Find("LevelTemplateStart").gameObject.transform.Find("Acid").gameObject.GetComponent<AcidFloor>().iterationMultiplier = i + 1;
+                    newLevel.transform.Find("LevelTemplateStart").gameObject.transform.Find("Ghost").gameObject.GetComponent<Ghost>().iterationMultiplier = i + 1;
+
+                    newLevel.transform.Find("LevelTemplateStart").gameObject.SetActive(true);
+                    newLevel.transform.Find("LevelTemplate3").gameObject.SetActive(false);
+                    newLevel.transform.Find("LevelTemplate2").gameObject.SetActive(false);
+                    newLevel.transform.Find("LevelTemplate1").gameObject.SetActive(false);
+                    newLevel.transform.Find("LevelTemplateEnd").gameObject.SetActive(false);
+
                 }
+                else if(i == 7)
+                {
+                    newLevel.transform.Find("LevelTemplateEnd").gameObject.transform.Find("Acid").gameObject.GetComponent<AcidFloor>().iterationMultiplier = i + 1;
+                    newLevel.transform.Find("LevelTemplateEnd").gameObject.transform.Find("Ghost").gameObject.GetComponent<Ghost>().iterationMultiplier = i + 1;
+
+                    newLevel.transform.Find("LevelTemplateEnd").gameObject.SetActive(true);
+                    newLevel.transform.Find("LevelTemplate3").gameObject.SetActive(false);
+                    newLevel.transform.Find("LevelTemplate2").gameObject.SetActive(false);
+                    newLevel.transform.Find("LevelTemplate1").gameObject.SetActive(false);
+                    newLevel.transform.Find("LevelTemplateStart").gameObject.SetActive(false);
+                }
+                else
+                {
+                    whichLevel = Random.Range(1, 4);
+                    newLevel.transform.Find("LevelTemplate" + whichLevel).gameObject.transform.Find("Acid").gameObject.GetComponent<AcidFloor>().iterationMultiplier = i + 1;
+                    newLevel.transform.Find("LevelTemplate" + whichLevel).gameObject.transform.Find("Ghost").gameObject.GetComponent<Ghost>().iterationMultiplier = i + 1;
+                    switch (whichLevel)
+                    {
+                        case 1:
+                            newLevel.transform.Find("LevelTemplate3").gameObject.SetActive(false);
+                            newLevel.transform.Find("LevelTemplate2").gameObject.SetActive(false);
+                            newLevel.transform.Find("LevelTemplate1").gameObject.SetActive(true);
+                            break;
+                        case 2:
+                            newLevel.transform.Find("LevelTemplate3").gameObject.SetActive(false);
+                            newLevel.transform.Find("LevelTemplate1").gameObject.SetActive(false);
+                            newLevel.transform.Find("LevelTemplate2").gameObject.SetActive(true);
+                            break;
+                        case 3:
+                            newLevel.transform.Find("LevelTemplate3").gameObject.SetActive(true);
+                            newLevel.transform.Find("LevelTemplate2").gameObject.SetActive(false);
+                            newLevel.transform.Find("LevelTemplate1").gameObject.SetActive(false);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                
             }
         }
     }
